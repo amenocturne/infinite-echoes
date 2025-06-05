a791b72f276587cb34ea16d764ed05693732c003
skril <you@example.com>
2025-06-05 11:53:09 +0300

    fix warning

--- web_client/webpack.config.js
+++ web_client/webpack.config.js
@@ -1,23 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'production',
+  entry: "./src/index.ts",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  performance: {
+    hints: "warning",
+    maxAssetSize: 1024 * 1024,
+    maxEntrypointSize: 1024 * 1024,
   },
 };
-
